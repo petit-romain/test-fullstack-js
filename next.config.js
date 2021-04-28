d66dbf66699d0ae03f910adc2879c71e50248097
petit-romain <petit.romain@outlook.fr>
2021-04-28 17:55:59 +0200

    Update: Render.yaml

--- next.config.js
+++ next.config.js
@@ -1,1 +1,33 @@
-module.exports = {}
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals)
+      ]
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader'
+      })
+    }
+    return config
+  }
+})
