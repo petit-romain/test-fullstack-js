33a16069b5f51bb37b528b56c1b1f90e7d473930
petit-romain <petit.romain@outlook.fr>
2021-05-04 14:22:44 +0200

    Feature: Primary color from environment variable

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withAntdLess = require('next-plugin-antd-less')
 
 module.exports = withAntdLess({
   // optional
-  modifyVars: { '@primary-color': '#696969' },
+  modifyVars: { '@primary-color': process.env.NEXT_PUBLIC_APP_COLOR },
   // optional
   lessVarsFilePathAppendToEndOfContent: false,
   // optional https://github.com/webpack-contrib/css-loader#object
