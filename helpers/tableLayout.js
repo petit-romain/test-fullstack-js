557826620abe2c9625236f1f91cc979605122894
petit-romain <petit.romain@outlook.fr>
2021-05-12 13:00:13 +0200

    Feature: Sorter and filters on column

--- helpers/tableLayout.js
+++ helpers/tableLayout.js
@@ -1,6 +1,4 @@
-import { defaultTo, find, keys, map, reject } from 'lodash'
-import React, { Fragment } from 'react'
-import { Tag } from 'antd'
+import { defaultTo, find, includes, isEmpty, keys, map, reject } from 'lodash'
 
 const sorter = (a = {}, b = {}, field) => {
   switch (field?.type) {
@@ -9,7 +7,7 @@ const sorter = (a = {}, b = {}, field) => {
     case 'Integer':
       return defaultTo(a?.key, 0) - defaultTo(b?.key, 0)
     default:
-      return null
+      return defaultTo(a?.key?.length, []) - defaultTo(b?.key?.length, [])
   }
 }
 
@@ -25,28 +23,22 @@ const filters = (field) => {
   }))
 }
 
-/* const field = find(model?.fields, ['name', column?.key])
-
-const isFieldSortable = includes(['String', 'Integer'], field?.type)
-const isFieldFilterable = !isEmpty(field?.choices) && field?.kind === 'enum'
-
-return {
-  ...column,
-  sorter: isFieldSortable ? (a, b) => sorter(a, b, field) : null,
-  filters: isFieldFilterable ? filter(field) : null
-} */
-
 export const formatColumns = (model, data, t) => {
-  const dataFields = keys(defaultTo(data?.results.pop(), {}))
+  const dataFields = keys(defaultTo(data?.results?.pop(), {}))
 
   return map(dataFields, (fieldKey) => {
     const field = find(defaultTo(model?.fields, []), ['name', fieldKey])
     const fieldName = defaultTo(field?.name, '')
 
+    const isFieldSortable = includes(['String', 'Integer'], field?.type)
+    const isFieldFilterable = !isEmpty(field?.choices) && field?.kind === 'enum'
+
     return {
       title: t(`fields.${fieldName}.title`),
       dataIndex: fieldName,
-      key: fieldName
+      key: fieldName,
+      sorter: isFieldSortable ? (a, b) => sorter(a, b, field) : null,
+      filters: isFieldFilterable ? filters(field) : null
     }
   })
 }
