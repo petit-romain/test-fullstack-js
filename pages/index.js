235050b546d4c92e6310ed4f58aa33e72743efcc
petit-romain <petit.romain@outlook.fr>
2021-05-14 09:43:31 +0200

    Update: Translation on home page

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,56 @@
+// Libraries
 import React, { useEffect } from 'react'
 import { getSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
 import { Spin } from 'antd'
 import { defaultTo, isNil } from 'lodash'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
+// Configs
+import i18nConfig from 'configs/i18n.config'
+
+// Styles
 import styles from './Home.module.css'
 
 const Home = ({ session }) => {
   const router = useRouter()
 
+  const { t } = useTranslation('Common')
+
   useEffect(() => {
-    router.push(
-      isNil(session)
-        ? defaultTo(process.env.NEXT_PUBLIC_APP_SIGNIN_PAGE, '/auth/signin')
-        : defaultTo(process.env.NEXT_PUBLIC_APP_HOME_PAGE, '/dashboard')
+    const signInUrl = defaultTo(
+      process.env.NEXT_PUBLIC_APP_SIGNIN_PAGE,
+      '/auth/signin'
     )
+    const landingUrl = defaultTo(
+      process.env.NEXT_PUBLIC_APP_HOME_PAGE,
+      '/dashboard'
+    )
+
+    router.push(isNil(session) ? signInUrl : landingUrl)
   }, [])
 
   return (
-    <div id={styles.page}>
-      <Spin tip='Chargement en cours...' />
+    <div className={styles.page}>
+      <Spin tip={t('loading')} />
     </div>
   )
 }
 
-export const getServerSideProps = async (context) => ({
-  props: {
-    session: await getSession(context)
+export const getServerSideProps = async ({ req, locale }) => {
+  const translations = await serverSideTranslations(
+    locale,
+    ['Common'],
+    i18nConfig
+  )
+
+  return {
+    props: {
+      session: await getSession({ req }),
+      ...translations
+    }
   }
-})
+}
 
 export default Home
