4c8eace10cba260da9dc05ffc8cbe93f4e3e906c
whazzark <petit.romain@outlook.fr>
2021-05-18 00:41:36 +0200

    Fix: English error page translations

--- components/error/Error.i18n.js
+++ components/error/Error.i18n.js
@@ -10,3 +10,14 @@ i18next.addResourceBundle('fr', 'Error', {
     description: 'Désolé, une erreur serveur interne est apparue'
   }
 })
+
+i18next.addResourceBundle('en', 'Error', {
+  '404': {
+    title: 'Error | Page not found',
+    description: 'Sorry, this page does not exist'
+  },
+  '500': {
+    title: 'Error | Internal server',
+    description: 'Sorry, an internal server error occurred'
+  }
+})
