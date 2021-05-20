49c417defa382e74654d44382a8e791605d1eb84
whazzark <petit.romain@outlook.fr>
2021-05-20 22:12:54 +0200

    Update: Input / Select / Datetime format

--- components/form/index.js
+++ components/form/index.js
@@ -1,7 +1,7 @@
 // Libraries
 import React, { useEffect } from 'react'
-import { Button, Card, Form, Input } from 'antd'
-import { defaultTo, isEmpty, map } from 'lodash'
+import { Button, Card, DatePicker, Form, Input, Select, TimePicker } from 'antd'
+import { defaultTo, includes, isEmpty, map } from 'lodash'
 
 // Helpers
 import { filterModelFields } from 'helpers/form'
@@ -37,7 +37,7 @@ const FormLayout = ({ t, model }) => {
         {map(modelFields, (field, index) => {
           const isFieldRequired = defaultTo(field?.isRequired, false)
 
-          const rules = [
+          let rules = [
             {
               required: isFieldRequired,
               message: t(`FormLayout:requiredMessage`, {
@@ -46,20 +46,54 @@ const FormLayout = ({ t, model }) => {
             }
           ]
 
+          if (includes(field?.name, 'mail') && field?.type === 'String') {
+            rules = [
+              ...rules,
+              {
+                type: 'email',
+                message: t(`FormLayout:patternMessage.email`, {
+                  fieldName: t(`fields.${field?.name}.title`).toLowerCase()
+                })
+              }
+            ]
+          }
+
+          const placeholder = t(`FormLayout:input.placeholder`, {
+            fieldName: t(`fields.${field?.name}.title`).toLowerCase()
+          })
+
           return (
             <Form.Item
               key={index}
+              name={field?.name}
               label={t(`fields.${field?.name}.title`)}
               rules={rules}
               required={isFieldRequired}
             >
               {/* Simple input field (String) */}
-              {field?.type === 'String' && (
-                <Input
-                  placeholder={t(`FormLayout:input.placeholder`, {
-                    fieldName: t(`fields.${field?.name}.title`).toLowerCase()
-                  })}
-                />
+              {field?.type === 'String' && <Input placeholder={placeholder} />}
+
+              {/* Enum field */}
+              {field?.kind === 'enum' && (
+                <Select
+                  allowClear
+                  mode={field?.isList ? 'multiple' : 'single'}
+                  placeholder={placeholder}
+                >
+                  {map(defaultTo(field?.choices, []), (choice) => (
+                    <Select.Option key={choice}>{choice}</Select.Option>
+                  ))}
+                </Select>
+              )}
+
+              {/* Datetime and Time field */}
+              {field?.type === 'DateTime' ||
+              includes(field?.name.toLowerCase(), 'time') ? (
+                <TimePicker placeholder={placeholder} />
+              ) : (
+                includes(field?.name.toLowerCase(), 'date') && (
+                  <DatePicker placeholder={placeholder} />
+                )
               )}
             </Form.Item>
           )
