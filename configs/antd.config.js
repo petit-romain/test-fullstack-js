0fc330c932f91b338c414001a0330c380eec03d9
petit-romain <petit.romain@outlook.fr>
2021-07-13 17:59:28 +0200

    Update: Default Antd primary color

--- configs/antd.config.js
+++ configs/antd.config.js
@@ -1,11 +1,13 @@
 const withAntdLess = require('next-plugin-antd-less')
-const { merge } = require('lodash')
+const { defaultTo, merge } = require('lodash')
 
 const webpackConfig = require('./webpack.config')
 
 module.exports = withAntdLess({
   // optional
-  modifyVars: { '@primary-color': process.env.NEXT_PUBLIC_APP_COLOR },
+  modifyVars: {
+    '@primary-color': defaultTo(process.env.NEXT_PUBLIC_APP_COLOR, '#696969')
+  },
   // optional
   lessVarsFilePathAppendToEndOfContent: false,
   // optional https://github.com/webpack-contrib/css-loader#object
